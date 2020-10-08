71b4169ace417a0e03df132338b8d3bbb91ad500
basir5101 <67514986+basir5101@users.noreply.github.com>
2020-10-08 22:08:09 +0600

    sample commit

---
+++ index.js
@@ -0,0 +1,12 @@
+// creating active class to nav links
+
+var navLinksContainer = document.getElementById("nav-links");
+var navLinks = navLinksContainer.getElementsByClassName("nav-link");
+
+for (var i = 0; i < navLinks.length; i++) {
+  navLinks[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
